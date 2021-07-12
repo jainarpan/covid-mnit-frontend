4da5e111444f66b10adc93f1b3cad1288ea4e6ad
Arpan Jain <arpangodha@gmail.com>
2021-07-12 21:30:22 +0530

    2

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
 window.env = {
-  "REACT_APP_SERVER_URL": "https://covidproject-backend.herokuapp.com/",
+  "REACT_APP_SERVER_URL": "https://dashboard.heroku.com/apps/covidproject-backend",
   "REACT_APP_SITE_URL": "http://localhost:3000/"
 };
